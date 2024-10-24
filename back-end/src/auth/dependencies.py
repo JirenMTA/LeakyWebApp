1ef803565189e571d57c576dbdea272934416095
JirenMTA <phamngochung9a@gmail.com>
2024-10-25 00:10:29 +0400

    Delete old

--- back-end/src/auth/dependencies.py
+++
@@ -1,8 +0,0 @@
-from fastapi import Request, HTTPException, status, Depends
-
-
-def get_token(request: Request):
-    token = request.cookies.get("auth")
-    if not token:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="No access token")
-    return token
--- back-end/src/config.py
+++
@@ -1,2 +0,0 @@
-def get_auth_data():
-    return {"secret_key": "MySuperSecretJwtKey1337", "algorithm": "HS256"}
