e4eaf6944a59e5584569c6baaf21a7777b7cae5c
JirenMTA <phamngochung9a@gmail.com>
2024-11-18 16:04:40 +0400

    Добавлен cors middleware в бэк

--- back-end/src/main.py
+++ back-end/src/main.py
@@ -29,7 +29,22 @@ async def lifespan(app: FastAPI):
     print("Приложение выключено!")
 
 
+
 app = FastAPI(title="Leaky Web App", lifespan=lifespan)
+
+origins = [
+    "http://localhost:3000",
+    "https://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(auth_router)
